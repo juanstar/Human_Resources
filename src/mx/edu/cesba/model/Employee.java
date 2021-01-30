3c9a4b201f36a6806d27a03775b0dcbb9c0f6c9f
j_sta <j_sta@LAPTOP-IBN7A47E>
2021-01-30 11:58:36 -0600

    Version 1.1

---
+++ src/mx/edu/cesba/model/Employee.java
@@ -0,0 +1,19 @@
+/*
+NOMBRE: JUAN LOPEZ ESTRELLA
+MATRICULA: 2023114
+ */
+package mx.edu.cesba.model;
+
+public class Employee {
+
+    public int id;
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int phoneNumber;
+    public double salary;
+    public String hiredate;
+
+    public void getDetails() { // Metodo
+    }
+}

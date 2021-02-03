6df83880cae2363b624d5e24fafd3ffd67c596f0
j_sta <j_sta@LAPTOP-IBN7A47E>
2021-02-02 23:50:32 -0600

    Version 1.1

---
+++ src/mx/edu/cesba/model/Deparment.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.cesba.model;
+
+/**
+ *
+ * @author j_sta
+ */
+public class Deparment {
+
+    public int id;
+    public String title;
+    public String location;
+
+    public void getDetails() { // Metodo
+    }
+}

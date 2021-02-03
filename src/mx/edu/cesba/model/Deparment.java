3627f96fa5b57f66031812f41f4e66e00cdd2e3a
j_sta <j_sta@LAPTOP-IBN7A47E>
2021-02-02 23:55:08 -0600

    Version 1.1

--- src/mx/edu/cesba/model/Deparment.java
+++ src/mx/edu/cesba/model/Deparment.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+NOMBRE: JUAN LOPEZ ESTRELLA
+MATRICULA: 2023114
  */
 package mx.edu.cesba.model;
 
---
+++ src/mx/edu/cesba/model/Job.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.cesba.model;
+
+/**
+ *
+ * @author j_sta
+ */
+public class Job {
+
+    public int id;
+    public String title;
+    public double minsalary;
+    public double maxsalary;
+
+    public void getDetails() { // Metodo
+    }
+
+}
